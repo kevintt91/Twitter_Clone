33f1dfd8ab7b3c91b8814e5045c0c0475d7d883a
Changhyun Kim <kevintt91@gmail.com>
2023-05-04 17:14:55 -0700

    Initial Commit

---
+++ Twitter-Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter-Clone
+//
+//  Created by 김창현 on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
